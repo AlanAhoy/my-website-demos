745de8d539fc6b3f3416af762fa1516ee3ea91b7
Alan <104424222+AlanAhoy@users.noreply.github.com>
2025-12-28 11:45:33 +0800

    Update vite.config.ts

--- demos/christmasTree/vite.config.ts
+++ demos/christmasTree/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
-    base: "/christmasTree/dist/", // ✅ 新增：打包后资源路径正确
+    base: "/my-website-demos/demos/christmasTree/dist/", // ✅ 新增：打包后资源路径正确
     server: {
       port: 3000,
       host: "0.0.0.0",
